54b634de49f1c73b1ae3eebe399cb5f7e47a8daa
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-06 16:42:07 +0530

    Add files via upload

---
+++ Swapp1.java
@@ -0,0 +1,21 @@
+ class Swapp1
+ {
+   public static void main(String[]args)
+    {
+     int a=14;
+     int b=47;
+     int c=0;
+     
+     c=a;
+     a=b;
+     b=c;
+     
+     System.out.println("before swapping");
+     System.out.println("a=14 b=47");
+     
+     System.out.println("after swap");
+     System.out.println("a=" +a);
+     System.out.println("b=" +b);
+    }
+ }
+   
